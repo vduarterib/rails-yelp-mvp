6bfc2c3bac94b54ca984a6f39a6ecfdf637fbb3f
Vasco Ribeiro <vduarterib@icloud.com>
2016-05-11 14:49:06 +0200

    models

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ActiveRecord::Base
+   has_many :reviews, dependent: :destroy
+
+   validates :category, inclusion: {in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
+   validates :name, uniqueness: true, presence: true
+   validates :address, presence: true
+   validates :phone_number, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "0176767757",
+    category:     "chinese",
+   },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "0176767757",
+    category:     "italian",
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "0176767757",
+    category:     "japanese",
+  },
+    {
+    name:         "La bouche",
+    address:      "Av Zola, 75016 Paris",
+    phone_number: "0176767757",
+    category:     "french",
+  },
+  {
+    name:         "La Loute",
+    address:      "rue de Berne 7508 Paris",
+    phone_number: "0176767757",
+    category:     "belgian",
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
